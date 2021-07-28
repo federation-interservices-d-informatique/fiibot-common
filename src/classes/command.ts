173dd53d502ffd65c3ded38e57c4c4d76626a7a8
Woomymy <github@woomy.ovh>
2021-07-28 19:19:17 +0200

    Commands: Add constructor

--- src/classes/command.ts
+++ src/classes/command.ts
@@ -1,1 +1,28 @@
-export class Command {}
+import { commandOptions } from "../lib";
+import { fiiClient } from "./client";
+
+/**
+ * Bot command
+ */
+export class Command {
+    /** Command options (name, description, ...) */
+    infos: commandOptions;
+    /** Discord client (we can also use message.client) */
+    client: fiiClient;
+    /** Command temp data */
+    data: Map<string, any>;
+    /**
+     * Create a new command (must be extended)
+     * @param client - The client
+     * @param options - Command options
+     */
+    constructor(
+        client: fiiClient,
+        options: commandOptions,
+        data?: Map<string, any>
+    ) {
+        this.client = client;
+        this.infos = options;
+        this.data = data || new Map<string, any>();
+    }
+}
