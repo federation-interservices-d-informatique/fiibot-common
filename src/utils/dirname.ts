d1f3a8a7354606cee116966a66fba9a627babd02
Woomy <woomy@woomy.be>
2021-07-28 14:24:57 +0200

    Utils: Document dirname

--- src/utils/dirname.ts
+++ src/utils/dirname.ts
@@ -1,4 +1,8 @@
 import { dirname } from "path";
+/**
+ * Get the dirname of the file. Replacement of __dirname (not aviable with es6 modules)
+ * @returns {string} - The dirname
+ */
 export const getDirname = (): string => {
     const mod_url = new URL(import.meta.url);
     return dirname(mod_url.pathname);
