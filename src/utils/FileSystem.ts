ba2545bdeaf74d6d5cddd7e169a949755bd218f6
Woomy <woomy@woomy.be>
2021-07-29 17:03:16 +0200

    Internals: Document walkDir

--- src/utils/FileSystem.ts
+++ src/utils/FileSystem.ts
@@ -1,12 +1,18 @@
 import { readdirSync } from "fs";
 import { lstatSync } from "fs";
-export function walkDir(path: string): string[] {
+/**
+ * Walk a dir recursivly
+ * @param path The path to scan
+ * @param filter extensions filter. Ex: .js
+ * @returns Path to files
+ */
+export function walkDir(path: string, filter: string = ".js"): string[] {
     const result = [];
     const files = readdirSync(path);
     files.forEach((file: string) => {
         if (
             !lstatSync(`${path}/${file}`).isDirectory() &&
-            file.endsWith(".js")
+            file.endsWith(filter)
         ) {
             result.push(`${path}/${file}`);
         } else if (lstatSync(file).isDirectory()) {
