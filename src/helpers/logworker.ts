dc16be8b3e4af7c69d5750ecff4c6554392766c2
Woomymy <github@woomy.ovh>
2021-07-29 17:06:09 +0200

    *: Try to fix CI

--- src/helpers/logworker.ts
+++ src/helpers/logworker.ts
@@ -1,10 +1,13 @@
 import { WebhookClient } from "discord.js";
 import { parentPort } from "worker_threads";
 
-const whclient = new WebhookClient({
-    id: `${BigInt(process.env.LOGS_WEBHOOK_ID)}`,
-    token: process.env.LOGS_WEBHOOK_TOKEN
-});
+const whclient = new WebhookClient(
+    {
+        id: `${BigInt(process.env.LOGS_WEBHOOK_ID)}`,
+        token: process.env.LOGS_WEBHOOK_TOKEN
+    },
+    {}
+);
 
 const postHook = (msg: string): void => {
     /* eslint-disable-next-line */
