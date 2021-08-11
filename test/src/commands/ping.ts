9da092a5a6d287d25c3e7477763e0e031450a3f0
Woomy <woomy@woomy.be>
2021-08-11 17:51:49 +0200

    TestClient: Refactor ping

--- test/src/commands/ping.ts
+++ test/src/commands/ping.ts
@@ -1,16 +1,15 @@
-import { Message } from "discord.js";
+import { CommandInteraction, Message } from "discord.js";
 import { fiiClient } from "../../../src";
 import { Command } from "../../../src/classes/command.js";
 
 export default class PingCommand extends Command {
     constructor(client: fiiClient) {
         super(client, {
-            name: "ping",
-            description: "Get bot's ping",
-            aliases: ["pong"]
-        });
+            name: 'ping',
+            description: 'Get bot ping'
+        }, {});
     }
-    async run(message: Message): Promise<void> {
-        message.reply("Heeeello");
+    async run(inter: CommandInteraction): Promise<void> {
+        inter.reply("Heeeello");
     }
 }
