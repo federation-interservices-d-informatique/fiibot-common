1c68971ce19239cdbb48a786db1b2d9b00e740c8
Woomymy <github@woomy.ovh>
2022-05-15 18:50:30 +0200

    Refactor: Add more comments and documentation and cleanup code

--- src/utils/random.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generate a random number
- * @param {number} min The minimum number
- * @param {number} max The maximum number
- * @returns {number} Random number between min and max
- */
-export const getRandom = (min: number, max: number): number => {
-    return Math.floor(Math.random() * (max - min) + min);
-};
