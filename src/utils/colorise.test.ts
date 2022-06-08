5d24fa9549fde82da02cbffd955a8ee91ef81c00
Woomymy <github@woomy.ovh>
2022-06-08 18:25:29 +0200

    Utils: Fix tests

--- src/utils/colorise.test.ts
+++
@@ -1,8 +0,0 @@
-import { colorise } from "./colorise";
-
-test("Colorise works", () => {
-    expect(colorise("Hello", "GREEN")).toBe("\u001b[32mHello\u001b[0m");
-});
-test("Default colorise is OK", () => {
-    expect(colorise("Hello")).toBe("\u001b[37mHello\u001b[0m");
-});
---
+++ src/utils/index.test.ts
@@ -0,0 +1,20 @@
+/**
+ * All tests for util
+ */
+import { colorise } from "./colorise";
+import { codeBlock } from "./format";
+import { getDirname, walkDir } from "./filesystem";
+
+/* Tests for colorise.ts */
+test("Colorise works", () => {
+    expect(colorise("Hello", "GREEN")).toBe("\u001b[32mHello\u001b[0m");
+});
+
+test("Default colorise is OK", () => {
+    expect(colorise("Hello")).toBe("\u001b[37mHello\u001b[0m");
+});
+
+/* Tests for format.ts */
+test("codeBlock function works", () => {
+    expect(codeBlock("markdown", "# Hello")).toBe("```markdown\n# Hello```");
+});
