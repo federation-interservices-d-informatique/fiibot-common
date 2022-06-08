35d5e48a3e40d65c482e1a3b107fe9e018aefe58
Woomymy <github@woomy.ovh>
2022-06-08 15:26:40 +0200

    Tests: Fix colorise.test.ts

--- src/utils/colorise.test.ts
+++ src/utils/colorise.test.ts
@@ -1,8 +1,8 @@
 import { colorise } from "./colorise";
 
 test("Colorise works", () => {
-    expect(colorise("Hello", "GREEN")).toBe("\u001b[32mHello\u001b[m");
+    expect(colorise("Hello", "GREEN")).toBe("\u001b[32mHello\u001b[0m");
 });
 test("Default colorise is OK", () => {
-    expect(colorise("Hello")).toBe("\u001b[37mHello\u001b[m");
+    expect(colorise("Hello")).toBe("\u001b[37mHello\u001b[0m");
 });
