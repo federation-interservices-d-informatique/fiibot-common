e1a2338409e4e9f91887409985483e7833642385
Woomy <woomy@woomy.be>
2022-07-20 14:55:04 +0200

    CI: Try to fix release publication
    Signed-off-by: Woomymy <woomy@woomy.be>

--- .github/bin/genlog.sh
+++ .github/bin/genlog.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
 DIRNAME="$(dirname $0)"
 
-$DIRNAME/changelog.sh > "${GITHUB_WORKSPACE}/changelog.txt"
+echo "- Writing changelog in ${GITHUB_WORKSPACE}/changelog.txt"
+
+bash $DIRNAME/changelog.sh > "${GITHUB_WORKSPACE}/changelog.txt"
 
