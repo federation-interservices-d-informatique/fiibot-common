27db5d0db10fccd9e1ddc80a7513d41b0fc147c4
Woomymy <woomy@woomy.be>
2025-08-31 12:53:06 +0200

    fix(eslint): Ignore dist and node_modules
    Change-Id: I9d0181d667a5444f8af35f89572f21e320f62a06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import { globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.strictTypeChecked,
     tseslint.configs.stylisticTypeChecked,
+    globalIgnores(["**/node_modules", "**/dist", "**/jest.config.js"]),
     {
         languageOptions: {
             parserOptions: {
